dd14a535664dfb321b0661a3f256197a815a35d0
KARTIK PANDEY <kartik0pandey00@gmail.com>
2026-09-04 17:14:51 +0530

    Added New Main Javascript File into the Repo

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import MaintenancePage from './MaintenancePage.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+async function bootstrap() {
+  let maintenanceOn = false
+  try {
+    const res = await fetch('/maintenance.flag', { cache: 'no-store' })
+    maintenanceOn = res.ok
+  } catch (e) {
+    maintenanceOn = false
+  }
+
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      {maintenanceOn ? <MaintenancePage /> : <App />}
+    </StrictMode>,
+  )
+}
+
+bootstrap()
